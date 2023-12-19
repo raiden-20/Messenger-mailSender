e21da4c84367ff36049ddf6a4a9e1e1997c5ad4c
undine_20 <lena_sheina@bk.ru>
2023-12-19 12:42:30 +0300

    change main queue

--- src/main/java/ru/vsu/cs/sheina/mailsender/configuration/RabbitQueues.java
+++ src/main/java/ru/vsu/cs/sheina/mailsender/configuration/RabbitQueues.java
@@ -1,5 +1,5 @@
 package ru.vsu.cs.sheina.mailsender.configuration;
 
 public class RabbitQueues {
-    public static final String mainQueue = "MAIL_SENDER_QUEUE";
+    public static final String mainQueue = "FROM_AUTH_TO_MAIL_QUEUE";
 }
